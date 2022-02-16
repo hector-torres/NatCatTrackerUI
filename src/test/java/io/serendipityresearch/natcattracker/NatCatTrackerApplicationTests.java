856a56167b59bdb755c8941aa9a499fad7448961
Hector Torres <blackarrow06@users.noreply.github.com>
2022-02-15 21:54:36 -0600

    Initial commit

---
+++ src/test/java/io/serendipityresearch/natcattracker/NatCatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.serendipityresearch.natcattracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatCatTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
